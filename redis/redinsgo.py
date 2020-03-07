0d68d88eed980d194c7b8d25f293706717491ac3
Evandro Matheus Schmitz <evandroschmitz2010@hotmail.com>
2020-03-06 21:13:34 -0300

    feat: adicionado função de jogo e vencedor
    Adicionado função de jogo, para simular o comportamento do bingo de sortei e pontuação.
    
    Adicionado função de resultado para mostrar os numeros sorteados, o vencedor e a cartela do mesmo.

--- redis/redinsgo.py
+++ redis/redinsgo.py
@@ -1,12 +1,14 @@
 import redis
 
-
 class Redinsgo:
 
     def __init__(self):
         self.redis = redis.Redis()
 
     def setup(self):
+        # limpando a base
+        self.redis.flushall()
+
         # fazer o set de numeros
         for i in range(1, 100):
             self.redis.sadd('setNumeros', i)
@@ -16,10 +18,10 @@ class Redinsgo:
             # criação dos usuário
             self.redis.hset(f'user:{user}', 'name', f'user{user}')
             self.redis.hset(f'user:{user}', 'bcartela', f'cartela:{user}')
-            self.redis.hset(f'user:{user}', 'bscore', f'score{user}')
+            self.redis.hset(f'user:{user}', 'bscore', f'score:{user}')
 
-            # criação do score
-            self.redis.zadd('score', {f'score{user}': 0})
+            # criação do score do usuário
+            self.redis.zadd('score', {f'score:{user}': 0})
 
             # criação da cartela
             for cartela in range (15):
@@ -30,8 +32,58 @@ class Redinsgo:
                 
                 self.redis.sadd(f'cartela:{user}', num)
 
-            print(f'criou o user{user}')
+    
+    def play(self):
+        jogando = True
+        # enquanto estiver ocorrendo o jogo
+        print('\nIniciando o Redinsgo!')
+        rodada = 0
+        while(jogando):
+            rodada += 1
+            # escolher um número aleatório
+            num = int(self.redis.srandmember('setNumeros'))
+            # remover ele das possibilidades
+            self.redis.srem('setNumeros', num)
+
+            # adiciona os sorteados em outro set
+            self.redis.sadd('setNumerosSorteados', num)
+
+            print(f'Numero da rodada {rodada}: {num}')
+
+            # percorrer os jogadores pra ver se alguém tem o número na cartela
+            for key in self.redis.scan_iter("user:*"):
+                cartela = self.redis.hget(key, 'bcartela')
+
+                contemNum = self.redis.sismember(cartela, num)
+                if contemNum:
+                    score = self.redis.hget(key, 'bscore')
+                    self.redis.zincrby('score', 1, score)
+                    pontuacao = int(self.redis.zscore('score', score))
+                    if pontuacao == 15:
+                        # Salva a chave do vencedor
+                        self.redis.sadd('vencedores', key)
+                        print('\nRedinsgo!!!')
+                        jogando = False
+
+
+    def show_results(self):
+        numerosSorteados = self.redis.smembers('setNumerosSorteados')
+        numerosSorteados = [int(i) for i in numerosSorteados]
+        print(f'\nNumeros Sorteados:{numerosSorteados}')
+
+        vencedores = self.redis.smembers('vencedores')
+        print('\nVencedores desta rodada de Redinsgo:')
+        for vencedor in vencedores:
+            name = self.redis.hget(vencedor, 'name').decode('utf-8')
+            print(f'{name}')
+            cartela = self.redis.hget(vencedor, 'bcartela')
+            cartela = self.redis.smembers(cartela)
+            cartela = [int(i) for i in cartela]
+            print(f'Cartela: {cartela}')
+            
 
 if __name__ == '__main__':
-    r = Redinsgo()
-    r.setup()
+    redinsgo = Redinsgo()
+    redinsgo.setup()
+    redinsgo.play()
+    redinsgo.show_results()
